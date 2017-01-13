b25085c8a834e20a22eb6db132641d1c35bfe44d
rudycrespo <rudy.crespo20@gmail.com>
2017-01-12 20:13:22 -0500

    background clip on hero text, moving background

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,5 @@
-console.log('\'Allo \'Allo!');
+const tagline = document.querySelector('.hero__tagline');
+
+document.querySelector('.page-hero').addEventListener('mousemove', function(e) {
+	tagline.style.backgroundPosition = `${-e.clientX}px ${-e.clientY}px`;
+});
