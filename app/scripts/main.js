838a2809c854e4c26ee09920b22d78a1e5fd2808
rudycrespo <rudy.crespo20@gmail.com>
2017-01-16 13:41:57 -0500

    experimenting with grays

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 // const tagline = document.querySelector('.hero__tagline');
 
-// document.querySelector('.page-hero').addEventListener('mousemove', function(e) {
-// 	tagline.style.backgroundPosition = `${-e.clientX}px ${-e.clientY}px`;
+// document.querySelector('.hero__tagline').addEventListener('mousemove', function(e) {
+// 	tagline.style.backgroundPosition = `${-e.clientX / 10 }px ${-e.clientY / 10 }px`;
 // });
