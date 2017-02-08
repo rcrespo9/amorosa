ea9449d9c1e9a30f071bdb93d28352ab65c8d8f2
rudycrespo <rudynoelcrespo@gmail.com>
2017-02-08 12:24:16 -0500

    working slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,11 +28,52 @@
 		}
 	}
 
+	class ProductSliders {
+		constructor() {
+			this.productTiles = $('#js-product-tiles');
+		}
+
+		prevSlide(e) {
+			const $sliderControl = $(e.target);
+
+			e.preventDefault();
+
+			$sliderControl
+				.closest('.slider__controls')
+				.siblings('.slider__img--active')
+				.prev('.slider__img')
+				.addClass('slider__img--active')
+				.next('.slider__img')
+				.removeClass('slider__img--active');
+		}
+
+		nextSlide(e) {
+			const $sliderControl = $(e.target);
+
+			e.preventDefault();
+
+			$sliderControl
+				.closest('.slider__controls')
+				.siblings('.slider__img--active')
+				.next('.slider__img')
+				.addClass('slider__img--active')
+				.prev('.slider__img')
+				.removeClass('slider__img--active');
+		}
+
+		init() {
+			this.productTiles.on('click', '.slider__control--right', this.nextSlide);
+			this.productTiles.on('click', '.slider__control--left', this.prevSlide);
+		}
+	}
+
 	class Main {
 		constructor() {
 			const productCards = new ProductCards();
+			const productSliders = new ProductSliders();
 
 			productCards.init();
+			productSliders.init();
 		}
 	}
 
