441415ea57d74b5ec7893b88814458e17df79071
rudycrespo <rudy.crespo20@gmail.com>
2017-02-09 12:58:26 -0500

    refactored slider controls

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,37 +33,37 @@
 			this.productTiles = $('#js-product-tiles');
 		}
 
-		prevSlide(e) {
+		changeSlide(e) {
 			const $sliderControl = $(e.target);
+			const sliderControlsParentSelector = '.slider__controls';
+			const sliderControlSelector = '.slider__control';
+			const sliderImgSelector = '.slider__img';
+			const sliderImgActiveSelector = '.slider__img--active';
+			const sliderImgActiveClass = 'slider__img--active';
 
 			e.preventDefault();
 
-			$sliderControl
-				.closest('.slider__controls')
-				.siblings('.slider__img--active')
-				.prev('.slider__img')
-				.addClass('slider__img--active')
-				.next('.slider__img')
-				.removeClass('slider__img--active');
-		}
-
-		nextSlide(e) {
-			const $sliderControl = $(e.target);
-
-			e.preventDefault();
-
-			$sliderControl
-				.closest('.slider__controls')
-				.siblings('.slider__img--active')
-				.next('.slider__img')
-				.addClass('slider__img--active')
-				.prev('.slider__img')
-				.removeClass('slider__img--active');
+			if($sliderControl.closest(sliderControlSelector).hasClass('slider__control--left')) {
+				$sliderControl
+					.closest(sliderControlsParentSelector)
+					.siblings(sliderImgActiveSelector)
+					.prev(sliderImgSelector)
+					.addClass(sliderImgActiveClass)
+					.next(sliderImgSelector)
+					.removeClass(sliderImgActiveClass);
+			} else if ($sliderControl.closest(sliderControlSelector).hasClass('slider__control--right')) {
+				$sliderControl
+					.closest(sliderControlsParentSelector)
+					.siblings(sliderImgActiveSelector)
+					.next(sliderImgSelector)
+					.addClass(sliderImgActiveClass)
+					.prev(sliderImgSelector)
+					.removeClass(sliderImgActiveClass);
+			}
 		}
 
 		init() {
-			this.productTiles.on('click', '.slider__control--right', this.nextSlide);
-			this.productTiles.on('click', '.slider__control--left', this.prevSlide);
+			this.productTiles.on('click', '.slider__control', this.changeSlide);
 		}
 	}
 
