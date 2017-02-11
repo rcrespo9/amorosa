81296bfe5f063d06386f1b55842b6646aafb467d
rudycrespo <rudy.crespo20@gmail.com>
2017-02-11 18:39:57 -0500

    instagram feed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -136,7 +136,7 @@
 		}
 
 		init() {
-			requestAnimationFrame(this.autoSlide.bind(this));
+			requestTimeout(this.autoSlide.bind(this), 5000);
 
 			if (Modernizr.touchevents) {
 			  this.productSlider.swipe({
@@ -164,6 +164,16 @@
 			sostenesSlider.init();
 			pantisSlider.init();
 			bathingSlider.init();
+
+			const userFeed = new Instafeed({
+			    get: 'user',
+			    userId: '2902294630',
+			    accessToken: '2902294630.1677ed0.0a71d20a11234d48be96415c27c2d116',
+			    resolution: 'standard_resolution',
+			    limit: 3,
+			    template: '<a class="instafeed__item" href="{{link}}" rel="noopener noreferrer" target="_blank"><img src="{{image}}" /></a>'
+			});
+			userFeed.run();
 		}
 	}
 
