3a584e7f822c41af99f9f370cff894e421eb0a97
rudycrespo <rudy.crespo20@gmail.com>
2017-02-12 01:07:14 -0500

    smaller instagram grid, more images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -170,7 +170,7 @@
 			    userId: '2902294630',
 			    accessToken: '2902294630.1677ed0.0a71d20a11234d48be96415c27c2d116',
 			    resolution: 'standard_resolution',
-			    limit: 3,
+			    limit: 20,
 			    template: '<a class="instafeed__item" href="{{link}}" rel="noopener noreferrer" target="_blank"><img src="{{image}}" /></a>'
 			});
 			userFeed.run();
