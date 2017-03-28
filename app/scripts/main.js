b624f8c44d04e04a0c06f0291f03f6bded9c9d67
rudycrespo <rudynoelcrespo@gmail.com>
2017-03-28 15:09:26 -0400

    instafeed overlay hover

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,7 @@
 				accessToken: '2902294630.1677ed0.0a71d20a11234d48be96415c27c2d116',
 				resolution: 'standard_resolution',
 				limit: 4,
-				template: '<li class="instafeed__item"><a class="instafeed__link" href="{{link}}" rel="noopener noreferrer" target="_blank"><img class="instafeed__img" src="{{image}}" alt="{{caption}}" /></a></li>'
+				template: '<li class="instafeed__item"><a class="instafeed__link" href="{{link}}" rel="noopener noreferrer" target="_blank"><div class="instafeed__overlay"><div class="instafeed__counters"><span class="instafeed__counter"><svg class="instafeed__icon"><use xlink:href="#icon-heart"></use></svg>{{likes}}</span><span class="instafeed__counter"><svg class="instafeed__icon"><use xlink:href="#icon-bubble"></use></svg>{{comments}}</span></div></div><img class="instafeed__img" src="{{image}}" alt="{{caption}}" /></a></li>'
 			});
 			userFeed.run();
 		}
